774d872ce255c2420e3a00a5ceffaea69a227520
Joseph Kowalski <jkjr25@gmail.com>
2022-04-26 14:14:06 -0400

    Made phase 1 add database and table if it doesn't exist

--- phase1/consumer-to-SQL.py
+++ phase1/consumer-to-SQL.py
@@ -5,20 +5,20 @@ from json import loads
 from sqlalchemy import create_engine, Column, Integer, String, table, insert
 from sqlalchemy.ext.declarative import declarative_base
 
-
+database = 'zipbank'
+user = getpass.getuser()
 password = getpass.getpass(stream=None)
-engine = create_engine('mysql+pymysql://joe:' + password + '@localhost/zipbank')
+db_engine = create_engine(f'mysql+pymysql://{user}:{password}@localhost')
+with db_engine.connect() as connect:
+    connect.execute(f'CREATE DATABASE IF NOT EXISTS {database}')
+    connect.execute(f'USE {database}')
+    connect.execute(f'CREATE TABLE IF NOT EXISTS transactions'
+                    f' (id INTEGER PRIMARY KEY AUTO_INCREMENT, '
+                    f'custid INTEGER, type VARCHAR(250) NOT NULL, '
+                    f'date INTEGER, '
+                    f'amt INTEGER)')
+engine = create_engine(f'mysql+pymysql://{user}:{password}@localhost/{database}')
 Base = declarative_base(bind=engine)
-
-class Transaction(Base):
-    __tablename__ = 'transactions'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    custid = Column(Integer)
-    type = Column(String(250), nullable=False)
-    date = Column(Integer)
-    amt = Column(Integer)
 
 class XactionConsumer:
     def __init__(self):
@@ -45,7 +45,7 @@ class XactionConsumer:
             self.ledger[message['custid']] = message
             # add message to the transaction table in your SQL usinf SQLalchemy
             with engine.connect() as connection:
-                connection.execute('insert into transactions (custid, type, date, amt) values(%s, %s, %s, %s)', (message['custid'], message['type'], message['date'], message['amt']))
+                connection.execute('insert into transactions ({0}, {1}, {2}, {3})'.format(message['custid'], message['type'], message['date'], message['amt']))
             if message['custid'] not in self.custBalances:
                 self.custBalances[message['custid']] = 0
             if message['type'] == 'dep':
